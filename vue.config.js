dd840ee86bc178164093c31d7fe54dfe0a78b24e
韩军 <1305544590@qq.com>
2019-09-28 15:51:56 +0800

    添加全局样式支持
    Signed-off-by: 韩军 <1305544590@qq.com>

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
+const path = require('path');
 module.exports = {
   pages: {
     index: './examples/main.js',
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname)
+      },
+    }
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "@/package/style/global_class.scss";
+        @import "@/package/style/global_var.scss";
+        `
+      }
+    }
   }
 }
